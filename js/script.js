26216cb7e2d76bc97a4a0af3391493530de374ad
Maksym-Maleiev <maximmaleev93@gmail.com>
2023-10-09 10:50:10 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 // Install Swiper
 new Swiper('.image-slider', {
-  // Navigation
+  // arrows
   navigation: {
-    nextEl: '.swiper-button-nwxt',
+    nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
+  },
+  // navigation
+  pagination: {
+    el: '.swiper-pagination',
+    // bullets
+    clickable: true,
   },
 });
